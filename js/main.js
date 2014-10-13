73770e336e8205d81c47e06507e7132d1c499c01
kok liu <kokdemo@gmail.com>
2014-10-14 00:06:55 +0800

    rewrite the add and sub of resource

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 /**
  * Created by JiaHao on 2014/10/10 0010.
  */
+/**
+ * vm是vuejs的核心对象，由于数据已经绑定，因此数据修改在vm.$data上完成。
+ * cm封装全局对象和方法，封装好的方法在jquery中直接执行之外，也供vuejs的方法调用。
+ * vm的方法中会封装一些页面上的方法。
+*/
+
 var vm = new Vue({
         el: 'body',
         data: {
@@ -18,11 +24,15 @@ var vm = new Vue({
                 happy: {storage: 0, maxStorage: 200, addSpeed: 0},
                 territory: {storage: 0, maxStorage: 200, addSpeed: 0}
             },
+            //人口 all是全部，分别有数量，最大数量，增加速度，每购买一个人的消耗，和维护一个人的损耗
             population: {
-                all: {storage: 0, maxStorage: 20, addSpeed: 0,
-                    cost: {wood: 10, food: 0, stone: 0, clothes: 0, medicine: 0, metal: 0, gold: 0, happy: 0, territory: 0}
-                },
-                consume: {cost: {wood: 0, food: 1, stone: 0, clothes: 0, medicine: 0, metal: 0, gold: 0, happy: 0, territory: 0}}
+                all: {
+                    storage: 0,
+                    maxStorage: 20,
+                    addSpeed: 0,
+                    cost: {wood: 10, food: 0, stone: 0, clothes: 0, medicine: 0, metal: 0, gold: 0, happy: 0, territory: 0},
+                    consume: {wood: 0, food: 1, stone: 0, clothes: 0, medicine: 0, metal: 0, gold: 0, happy: 0, territory: 0}
+                }
 
             }
 
@@ -31,62 +41,63 @@ var vm = new Vue({
         },
         methods: {
             addResource: function (resource, efficiency) {
-                if (resource['storage'] < resource['maxStorage']) {
-                    resource['storage'] += efficiency;
-                }
+                cm.addResource(resource, efficiency);
             },
             subtractResource: function (type, number) {
-                var temp = vm.$data.resource;
-                var resourceList = ['wood', 'food', 'stone', 'clothes', 'medicine', 'metal', 'gold', 'happy', 'territory'];
-                var check = 0;
-                for (var i = 0; i < resourceList.length; i++) {
-                    console.info(resourceList[i] + "||" + temp[resourceList[i]]);
-                    var tempObject = temp[resourceList[i]];
-                    if (tempObject.storage - type.cost[resourceList[i]] * number >= 0) {
-                        check++;
-                    }
-                }
-                if (check == 9) {
-                    for (var i = 0; i < resourceList.length; i++) {
-                        temp[resourceList[i]].storage -= type.cost[resourceList[i]] * number;
-                    }
-                    return 1
-                } else {
-                    return 0
-                }
+                cm.subtractResource(type,number);
             },
+            //购买人口
             addPeople: function (type, number) {
                 if (type['storage'] < type['maxStorage']) {
-                    if (this.subtractResource(type, number) == 1) {
+                    if (cm.ifEnoughResource(type, number) == cm.resourceList.length) {
                         type['storage'] += number;
+                        cm.subtractResource(type, number)
+                        for (var i = 0; i < cm.resourceList.length; i++) {
+                            //计算人口损耗
+                            vm.$data.resource[cm.resourceList[i]].addSpeed -= type.consume[cm.resourceList[i]] * number;
+                        }
                     }
                 }
             }
-
         }
     })
     ;
-
+ var show = vm.$data.resource;
 var cm = {
+    resourceList:['wood', 'food', 'stone', 'clothes', 'medicine', 'metal', 'gold', 'happy', 'territory'],
+    //资源增加
     addResource: function (resource, efficiency) {
         if (resource['storage'] < resource['maxStorage']) {
             resource['storage'] += efficiency;
         }
     },
-    subtractResource: function (type, number) {
+    //资源减少
+    subResource: function (resource,efficiency) {
+        if (resource['storage'] > 0){
+            resource['storage'] -= efficiency;
+        }
+    },
+    //资源减少，有校验，可以认为是资源购买过程
+    ifEnoughResource: function(type,number){
         var temp = vm.$data.resource;
-        var resourceList = ['wood', 'food', 'stone', 'clothes', 'medicine', 'metal', 'gold', 'happy', 'territory'];
         var check = 0;
-        for (var i = 0; i < resourceList.length; i++) {
-            console.info(resourceList[i] + "||" + temp[resourceList[i]]);
-            var tempObject = temp[resourceList[i]];
-            if (tempObject.storage - type.cost[resourceList[i]] * number >= 0) {
+        var resLength = cm.resourceList.length;
+        for (var i = 0; i < resLength; i++) {
+            var tempObject = temp[cm.resourceList[i]];
+            if (tempObject.storage - type.cost[cm.resourceList[i]] * number >= 0) {
                 check++;
             }
         }
-        if (check == 9) {
-            for (var i = 0; i < resourceList.length; i++) {
-                temp[resourceList[i]].storage -= type.cost[resourceList[i]] * number;
+        return check
+    },
+    subtractResource: function (type, number) {
+        var temp = vm.$data.resource;
+        var resLength = cm.resourceList.length;
+        var check = this.ifEnoughResource(type,number);
+        console.info(check);
+        if (check == resLength) {
+            for (var i = 0; i < resLength; i++) {
+                temp[cm.resourceList[i]].storage -= type.cost[cm.resourceList[i]] * number;
             }
             return 1
         } else {
@@ -94,13 +105,22 @@ var cm = {
         }
     },
     liquidateResource: function () {
-        var temp = vm.$data;
-        var popObject = temp.population.all;
+        var temp = vm.$data.resource;
             setInterval(function () {
-                var checkSub = cm.subtractResource(temp.population.consume, popObject.storage);
+                for (var i = 0; i < cm.resourceList.length; i++) {
+                    var tempResource =temp[cm.resourceList[i]];
+                    //cm.subResource(temp[cm.resourceList[i]].storage,temp[cm.resourceList[i]].addSpeed);
+                    if(tempResource.storage > 0){
+                        temp[cm.resourceList[i]].storage += tempResource.addSpeed;
+                    }
+                    //temp[cm.resourceList[i]].storage += temp[cm.resourceList[i]].addSpeed;
+                }
             }, 1000);
+    },
+    init: function () {
+        this.liquidateResource();
     }
 };
 $(document).ready(function(){
-    cm.liquidateResource();
+    cm.init();
 });
